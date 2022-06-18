f079b0568a1e1c0a34ca421dfa112dd0fa5557ff
zasssou <zassou.tnk@gmail.com>
2022-06-18 20:47:36 +0900

    add:フェードアニメーションを追加

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,47 @@
-$(function() {
+window.onload = function() { 
+  loadTopMessage();
+  fadeEffect();
+
+  $(window).scroll(() => {
+    fadeEffect();
+  });
 
   $('.button-menu').on('click', () => {
     $('.button-menu').toggleClass('button-menu--open');
     $('#header').toggleClass('header--expand');
   });
 
-});
+  $('.header-menu__link').on('click', () => {
+    console.log('aaaa')
+    if ($('.button-menu').hasClass('button-menu--open')) {
+      $('.button-menu').removeClass('button-menu--open');
+    }
+    if ($('#header').hasClass('header--expand')) {
+      $('#header').removeClass('header--expand');
+    }
+  });
+}
+
+async function sleep(delay) {
+  await new Promise(resolve => setTimeout(resolve, delay));
+};
+
+async function loadTopMessage() {
+  $('.fade-symbol').addClass('view');
+  await sleep(500);
+  $('.fade-title').addClass('view');
+  await sleep(500);
+  $('.fade-introduction').addClass('view');
+  
+}
+
+function fadeEffect() {
+  $('.fade').each(function() {
+    const elementPosition = $(this).offset().top;
+    const scrollPosition = $(window).scrollTop();
+    const windowHeight = $(window).height();
+    if (scrollPosition > elementPosition - windowHeight) {
+      $(this).addClass('view');
+    }
+  });
+}
