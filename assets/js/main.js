cff32ec750d0924f57ab30f7d213eb8d0540ee77
zasssou <zassou.tnk@gmail.com>
2022-06-18 09:31:02 +0900

    add:問い合わせフォーム追加

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ $(function() {
 
   $('.button-menu').on('click', () => {
     $('.button-menu').toggleClass('button-menu--open');
-    $('.nav-expand').toggleClass('nav-expand--open');
+    $('#header').toggleClass('header--expand');
   });
 
 });
